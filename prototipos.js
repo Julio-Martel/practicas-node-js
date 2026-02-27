cc70116614a7fd3a979b8b3949a918cb58764962
Julio-Martel <julioramdom123@gmail.com>
2026-02-27 18:33:34 -0300

    se han creado archivos para la practica de node

---
+++ prototipos.js
@@ -0,0 +1,27 @@
+const obtenerVidejuego = require('./mod1');
+let nombreDelJuego = 'Medal of Honor';
+
+if(obtenerVidejuego(nombreDelJuego)){
+    console.log(`${nombreDelJuego}, esta en la base de datos`);
+} else {
+    console.log(`${nombreDelJuego}, no existe`);
+}
+
+console.log("Inicio");
+
+setTimeout(() => {
+    console.log("Esto tarda 2 segundos");
+}, 2000);
+
+console.log("Fin");
+
+const fs = require("fs");
+
+fs.readFile("texto.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log("Error");
+        return;
+    }
+
+    console.log(data);
+});
